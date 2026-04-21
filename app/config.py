5c0e88d7c524a98ecd4d792ede2885bcdbe961e6
Gustavo Abreu <ga424@njit.edu>
2026-04-21 19:21:44 -0400

    feat(database): update database name and connection strings to module12

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:postgres@localhost:55432/module11_db"
+    DATABASE_URL: str = "postgresql://postgres:postgres@localhost:55432/module12_db"
     model_config = SettingsConfigDict(env_file=("local.env", ".env"))
 
 
